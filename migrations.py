63d7ff409d19f1dc486eb65ed51e6b43a98d0109
Dev Chitara <devchitara2003@gmail.com>
2024-03-10 21:55:14 +0530

    Created group model

--- migrations.py
+++ migrations.py
@@ -1,1 +1,4 @@
 from db_setup import engine, Base
+from models import users, posts, groups, comments, base
+
+Base.metadata.create_all(bind=engine)
--- models/groups.py
+++ models/groups.py
@@ -1,6 +1,8 @@
 import uuid
 from sqlalchemy import Column, String, UUID, Text, ForeignKey
+from sqlalchemy.orm import Relationship
 from base import TimeStamp
+from relationships import member_users, mods_users
 
 
 class Group(TimeStamp):
@@ -9,6 +11,10 @@ class Group(TimeStamp):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String(40), nullable=False)
     description = Column(Text, nullable=False)
+    owner_id = Column(UUID, ForeignKey("users.id", ondelete="CASCADE"))
+    
+    members = Relationship("User", secondary=member_users, backref="related_users")
+    mods = Relationship("User", secondary=mods_users, backref="related_mods")
 
     def __str__(self):
         return f"{self.name}"
