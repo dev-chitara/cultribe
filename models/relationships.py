5f8a3a115990c10f335d2e824b1c03c9bccc88ff
Dev Chitara <devchitara2003@gmail.com>
2024-03-10 22:04:07 +0530

    Created user model

---
+++ models/relationships.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, UUID, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from db_setup import Base
+
+member_users = Table(
+    "member_users",
+    Base.metadata,
+    Column("group_id", UUID, ForeignKey("groups.id"), primary_key=True),
+    Column("user_id", UUID, ForeignKey("users.id"), primary_key=True)
+)
+
+mods_users = Table(
+    "mods_users",
+    Base.metadata,
+    Column("group_id", UUID, ForeignKey("groups.id"), primary_key=True),
+    Column("user_id", UUID, ForeignKey("users.id"), primary_key=True)
+)
