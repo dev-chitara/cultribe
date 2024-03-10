b6855ea1e14dec1cfe0ff29e2209b77a9a6c339d
Dev Chitara <devchitara2003@gmail.com>
2024-03-10 23:35:29 +0530

    Created all models

--- models/comments.py
+++ models/comments.py
@@ -7,10 +7,8 @@ class Comment(TimeStamp):
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     content = Column(Text, nullable=False)
-
     owner_id = Column(UUID, ForeignKey("users.id", ondelete="CASCADE"))
     post_id = Column(UUID, ForeignKey("posts.id", ondelete="CASCADE"))
-
 
     def __str__(self):
         return f"{self.content}"
